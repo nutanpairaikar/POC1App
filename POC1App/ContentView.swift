df219e0fbf6dbbcc2ef4e44f307a2437b18c1b7e
Nutan Pai Raikar <nutanpairaikar@yahoo.com>
2021-09-28 09:59:44 -0400

    Initial Commit

---
+++ POC1App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  POC1App
+//
+//  Created by Nutan Pai Raikar on 9/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POC1App/POC1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC1AppApp.swift
+//  POC1App
+//
+//  Created by Nutan Pai Raikar on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct POC1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
